ef994825f2054eff6a5e45e5cc59caa3eaac4bac
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-10-21 21:57:31 +0500

    5-dars

--- index.js
+++ index.js
@@ -1,206 +1,145 @@
-// 1-masala
-// let arr = [4, 9, 2, 8, 5];
-
-// let res = [];
-
-// for (let i = 0; i < arr.length; i++) {
-//   res.push(arr[i]) * 2;
-// }
-
-// console.log(res);
-
-// 2-masala
-// let arr = [4, 5, 53, 2, 4];
-// let res = [];
-
-// for (let i = 0; i < arr.length; i++) {
-//   res.unshift(arr[i]);
-// }
-
-// console.log(res);
-
-// 4-misol
-
-// let arr = [1, 2, 3 , 4, 5];
-// let res = 0;
-
-// for(let i = 0; i < num.length; i++ ) {
-//     sum += num[i];
-// }
-// console.log(sum);
-
-
-// 5-misol
-
-// let arr = [1, 2, 3, 4, 5];
-// num.push(6);
-// console.log(num);
-
-
-// 6-misol
-// let arr = [1, 2, 3, 4, 5];
-// num.pop(6);
-// console.log(num);
-
-// 7-misol
-
-// let arr = [1, 2, 3, 4, 5];
-// num.shift();
-// console.log(num);
-
-
-// 8-misol
-// let arr = [1, 2, 3, 4, 5];
-// num.unshift(0);
-// console.log(num);
-
-
-// 9-misol
-
-// let res = [1, 2, 3, 4, 5];
-// arr = num[3];
-// console.log(arr);
-
-
-// 10-misol
-
-// let arr = [1, 2, 3, 4, 5];
-// let res = [];
-// for (let i = 0; i < num.length; i++) {
-//   arr.push(num[i]);
-// }
-// console.log(arr);
-
-
-// 11-misol
-
-// let arr = [1, 2, 3, 4, 5];
-// let res = num[0];
-
-// for (let i = 1; i < num.length; i++) {
-//   if (num[i] > max) {
-//     max = num[i];
-//   }
-// }
-// console.log(max);
-
-
-// 12-misol
-
-// let arr = [1, 2, 3, 4, 5, 6];
-// let res = num[0];
-
-// for(let i = 1; i > num.length; i--) {
-//     if (num[i] < min) {
-//         min = num[i];
-//     }
-// }
-// console.log(min);
-
-
-// 13-misol 
-
-// let arr = [1, 2, 3, 4, 5, 6];
-// let res = 0;
-
-// for(let i = 1; i < num.length; i++) {
-//     if(num[i] % 2 == 0) {
-//         count += 1;
-//     }
-// }
-// console.log(count);
-
-
-// 14-misol 
-
-// let arr = [1, 2, 3, 4, 5, 6];
-// let res = 0;
-
-// for(let i = 1; i < num.length; i++) {
-//     if(num[i] % 2 == 1) {
-//         count += 1;
-//     }
-// }
-// console.log(count);
-
-
-// 15-misol
-
-// let arr = [1, 0, 0, 4, 5, 0];
-// let res = [];
-
-// for(let i = 0; i < num.length; i++) {
-//     if(num[i] == 0) {
-//         continue;
-//     }
-//     arr.push(num[i])
-// }
-// console.log(arr);
-
-
-// 18-misol
-
-// let arr= [1, 2, 3, 4, 5];
-// let res = 1;
-
-// for (let i = 0; i < num.length; i++) {
-//     sum = sum * num[i];
-// }
-// console.log(sum);
-
-
-// 19-misol 
-
-// let arr = [1, 2, 3, 4, 5];
-// let res = [];
-
-// for (let i = 0; i < num.length; i += 2) {
-//     arr.push(num[i]);
-// }
-// console.log(arr);
-
-
-// 20-misol
-
-// let arr = [1, 2, 3, 4, 5, 6];
-// let res = [];
-
-// for(let i = 0; i < num.length; i++) {
-//     if(num[i] % 2 == 0) {
-//         arr.push(num[i])
-//     }
-// }
-// console.log(arr);
-
-
-// 21-misol
-
-// let arr = [1, 'salom', 'hayr', 2];
-// let res = [];
-
-// for(let i = 0; i < num.length; i++) {
-//     if(typeof num[i] == 'string') {
-//         arr.push(num[i])
-//     }
-// }
-// console.log(arr);
-
-
-// 22-misol
-
-// let arr = ['qalesan', 3, 34, 'salom'];
-// let res = [];
-
-// for(let i = 0; i < num.length; i++) {
-//     if(typeof num[i] == 'number') {
-//         arr.push(num[i])
-//     }
-// }
-// console.log(arr);
-
-
-// 23-misol
-
-// let num = [1, 2, 3, 4, 5];
-// let arr = num.length;
-// console.log(arr);
+//1-masala
+//let arr = [4, 9, 2, 8, 5];
+//let res = [];
+//for (let i = 0; i < arr.length; i++) {
+//  res.push(arr[i]) * 2;
+//}
+//console.log(res);
+//2-masala
+//let arr = [4, 5, 53, 2, 4];
+//let res = [];
+//
+//for (let i = 0; i < arr.length; i++) {
+//  res.unshift(arr[i]);
+//}
+//console.log(res);
+//4-misol
+//let arr = [1, 2, 3 , 4, 5];
+//let res = 0;
+//for(let i = 0; i < num.length; i++ ) {
+//    sum += num[i];
+//}
+//console.log(sum);
+//5-misol
+//let arr = [1, 2, 3, 4, 5];
+//num.push(6);
+//console.log(num);
+//6-misol
+//let arr = [1, 2, 3, 4, 5];
+//num.pop(6);
+//console.log(num);
+//7-misol
+//let arr = [1, 2, 3, 4, 5];
+//num.shift();
+//console.log(num);
+//8-misol
+//let arr = [1, 2, 3, 4, 5];
+//num.unshift(0);
+//console.log(num);
+//9-misol
+//let res = [1, 2, 3, 4, 5];
+//arr = num[3];
+//console.log(arr);
+//10-misol
+//let arr = [1, 2, 3, 4, 5];
+//let res = [];
+//for (let i = 0; i < num.length; i++) {
+//  arr.push(num[i]);
+//}
+//console.log(arr);
+//11-misol
+//let arr = [1, 2, 3, 4, 5];
+//let res = num[0];
+//for (let i = 1; i < num.length; i++) {
+//  if (num[i] > max) {
+//    max = num[i];
+//  }
+//}
+//console.log(max);
+//12-misol
+//let arr = [1, 2, 3, 4, 5, 6];
+//let res = num[0];
+//for(let i = 1; i > num.length; i--) {
+//    if (num[i] < min) {
+//        min = num[i];
+//    }
+//}
+//console.log(min);
+//13-misol 
+//let arr = [1, 2, 3, 4, 5, 6];
+//let res = 0;
+//for(let i = 1; i < num.length; i++) {
+//    if(num[i] % 2 == 0) {
+//        count += 1;
+//    }
+//}
+//console.log(count);
+//14-misol 
+//let arr = [1, 2, 3, 4, 5, 6];
+//let res = 0;
+//for(let i = 1; i < num.length; i++) {
+//    if(num[i] % 2 == 1) {
+//        count += 1;
+//    }
+//}
+//console.log(count);
+//15-misol
+//let arr = [1, 0, 0, 4, 5, 0];
+//let res = [];
+//
+//for(let i = 0; i < num.length; i++) {
+//    if(num[i] == 0) {
+//        continue;
+//    }
+//    arr.push(num[i])
+//}
+//console.log(arr);
+//18-misol
+//let arr= [1, 2, 3, 4, 5];
+//let res = 1;
+//
+//for (let i = 0; i < num.length; i++) {
+//    sum = sum * num[i];
+//}
+//console.log(sum);
+//19-misol 
+//let arr = [1, 2, 3, 4, 5];
+//let res = [];
+//for (let i = 0; i < num.length; i += 2) {
+//    arr.push(num[i]);
+//}
+//console.log(arr);
+//20-misol
+//let arr = [1, 2, 3, 4, 5, 6];
+//let res = [];
+//
+//for(let i = 0; i < num.length; i++) {
+//    if(num[i] % 2 == 0) {
+//        arr.push(num[i])
+//    }
+//}
+//console.log(arr);
+//21-misol
+//let arr = [1, 'salom', 'hayr', 2];
+//let res = [];
+//
+//for(let i = 0; i < num.length; i++) {
+//    if(typeof num[i] == 'string') {
+//        arr.push(num[i])
+//    }
+//}
+//console.log(arr);
+//22-misol
+//let arr = ['qalesan', 3, 34, 'salom'];
+//let res = [];
+//
+//for(let i = 0; i < num.length; i++) {
+//    if(typeof num[i] == 'number') {
+//        arr.push(num[i])
+//    }
+//}
+//console.log(arr);
+//23-misol
+//let num = [1, 2, 3, 4, 5];
+//let arr = num.length;
+//console.log(arr);
